fc8359137d8492976d34af58aa271315d42dfa2d
hasantalukder1234 <hasantalukdercou@gmail.com>
2023-11-12 20:52:07 +0600

    Delete Api created

--- index.js
+++ index.js
@@ -54,6 +54,14 @@ async function run() {
 
     })
 
+    app.delete('/bookings/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)}
+      const result = await bookingsCollection.deleteOne(query);
+      res.send(result);
+
+    })
+
     app.get('/bookings', async(req, res) => {
       let query = {}
        if(req.query?.email){
